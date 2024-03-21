bd04cfe8761b75c9af6d815b05df2e429bc5d521
Adrian Lora Garcia <adrian.loragarcia@berger-levault.com>
2024-03-21 08:07:16 +0100

    Remove comments

--- svcInstall.js
+++ svcInstall.js
@@ -9,12 +9,8 @@ var svc = new Service({
     '--harmony',
     '--max_old_space_size=4096'
   ]
-  //, workingDirectory: '...'
-  //, allowServiceLogon: true
 });
 
-// Listen for the "install" event, which indicates the
-// process is available as a service.
 svc.on('install',function(){
   svc.start();
 });
